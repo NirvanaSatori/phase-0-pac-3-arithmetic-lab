6cd34e67cebefa0151bf9567d8177ba13eec2ac5
MORRIS <morris24kariuki@gmail.com>
2022-11-19 20:43:39 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,65 @@
+function add(x, y) {
+    console.log(3 +4);
+    return add;
+}
+
+
+function subtract(x, y) {
+    console.log(3-4);
+    return subtract;
+}
+
+
+function multiply(x, y) {
+    console.log(3*4);
+    return multiply;
+}
+
+
+function divide(x, y) {
+    console.log(3/4);
+    return divide;
+}
+
+
+function add(a, b) {
+    return a+ b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+n = 2
+function increment(n){
+    return (n += 1);
+}
+increment();
+
+n = 2
+function decrement(n){
+    return (n -= 1);
+}
+decrement();
+
+n=9.897
+function makeInt(n){
+    n=8.99
+    return parseInt("n", 10);
+}
+makeInt();
+
+a = 2.567
+function preserveDecimal(n) {
+    parseFloat("2.567");
+    return preserveDecimal;
+}
+preserveDecimal();
